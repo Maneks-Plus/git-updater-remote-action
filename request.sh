08ca2dd04c888605e400fc8b085c2a01bc545f95
ilion227 <janliber1@gmail.com>
2021-08-20 19:26:39 +0200

    Added exit code

--- request.sh
+++ request.sh
@@ -1,4 +1,8 @@
-echo "START REQUEST"
-curl $1
-echo
-echo "END REQUEST"
+response_code=$(curl --max-time 6 --write-out %{http_code} --silent --output /dev/null "$1")
+if [[ "$response_code" -ne 200 ]] ; then
+    # Something went wrong, echo non-zero number to indicate failure
+    echo 1
+else
+    # Git Updater sucessfully ran on Wordpress, echo 0 to indicate success
+    echo 0
+fi
